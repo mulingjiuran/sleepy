71ef451d1c67af910996b85bbb9a7ce4c407dc1d
ran MO <mulingjiuran@qq.com>
2025-03-19 16:32:20 +0800

    Initial commit

---
+++ env.py
@@ -0,0 +1,84 @@
+# coding: utf-8
+import os
+import utils as u
+from dotenv import load_dotenv, find_dotenv
+dotenv_filename = '.env'
+if not find_dotenv(filename=dotenv_filename):
+    u.warning("未找到 .env 文件，将使用默认配置，部分功能可能失效！")
+load_dotenv(dotenv_path=dotenv_filename)
+
+
+def getenv(key: str, default, typeobj: object = str) -> any:
+    '''
+    获取环境变量
+
+    :param key: 键
+    :param typeobj: 类型对象 (str / int / bool)
+    :param default: 默认值 (未读取到此项配置时使用)
+    '''
+    got_value = os.getenv(key)
+    if got_value is None:
+        return default
+    else:
+        if typeobj == bool:
+            return u.tobool(got_value)
+        return typeobj(got_value)
+
+
+class _main:
+    '''
+    (main) 系统基本配置
+    '''
+    host: str = getenv('sleepy_main_host', '0.0.0.0', str)
+    port: int = getenv('sleepy_main_port', 9010, int)
+    flask_debug: bool = getenv('sleepy_main_flask_debug', False, bool)
+    timezone: str = getenv('sleepy_main_timezone', 'Asia/Shanghai', str)
+    checkdata_interval: int = getenv('sleepy_main_checkdata_interval', 30, int)
+    secret: str = getenv('SLEEPY_SECRET', '', str)
+
+
+class _page:
+    '''
+    (page) 页面内容配置
+    '''
+    title: str = getenv('sleepy_page_title', 'User Alive?', str)
+    desc: str = getenv('sleepy_page_desc', 'User\'s Online Status Page', str)
+    favicon: str = getenv('sleepy_page_favicon', '', str)
+    user: str = getenv('sleepy_page_user', 'User', str)
+    background: str = getenv('sleepy_page_background', 'https://imgapi.siiway.top/image', str)
+    learn_more: str = getenv('sleepy_page_learn_more', 'GitHub Repo', str)
+    repo: str = getenv('sleepy_page_repo', 'https://github.com/wyf9/sleepy', str)
+    more_text: str = getenv('sleepy_page_more_text', '', str)
+    hitokoto: bool = getenv('sleepy_page_hitokoto', True, bool)
+    canvas: bool = getenv('sleepy_page_canvas', True, bool)
+    moonlight: bool = getenv('sleepy_page_moonlight', True, bool)
+    lantern: bool = getenv('sleepy_page_lantern', False, bool)
+    mplayer: bool = getenv('sleepy_page_mplayer', True, bool)
+
+
+class _status:
+    '''
+    (status) 页面状态显示配置
+    '''
+    device_slice: int = getenv('sleepy_status_device_slice', 30, int)
+    show_loading: bool = getenv('sleepy_status_show_loading', True, bool)
+    refresh_interval: int = getenv('sleepy_status_refresh_interval', 5000, int)
+    not_using: str = getenv('sleepy_status_not_using', '', str)
+
+
+class _util:
+    '''
+    (util) 可选功能
+    '''
+    metrics: bool = getenv('sleepy_util_metrics', True, bool)
+    auto_switch_status: bool = getenv('sleepy_util_auto_switch_status', True, bool)
+    steam_legacy_enabled: bool = getenv('sleepy_util_steam_legacy_enabled', False, bool)
+    steam_enabled: bool = getenv('sleepy_util_steam_enabled', False, bool)
+    steam_key: str = getenv('sleepy_util_steam_key', '', str)
+    steam_ids: str = getenv('sleepy_util_steam_ids', '', str)
+
+
+main = _main()
+page = _page()
+status = _status()
+util = _util()
---
+++ server.py
@@ -0,0 +1,492 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import time
+import json5
+# import importlib - ready for plugin?
+import pytz
+import flask
+from datetime import datetime
+from markupsafe import escape
+
+import utils as u
+import env
+from data import data as data_init
+from setting import status_list
+
+try:
+    # init flask app
+    app = flask.Flask(__name__,template_folder='static')
+
+    # disable flask access log (if not debug)
+    if not env.main.flask_debug:
+        from logging import getLogger
+        flask_default_logger = getLogger('werkzeug')
+        flask_default_logger.disabled = True
+
+    # init data
+    d = data_init()
+    d.load()
+    d.start_timer_check(data_check_interval=env.main.checkdata_interval)  # 启动定时保存
+
+    # init metrics if enabled
+    if env.util.metrics:
+        u.info('[metrics] metrics enabled, open /metrics to see your count.')
+        d.metrics_init()
+except Exception as e:
+    u.error(f"Error initing: {e}")
+    exit(1)
+except KeyboardInterrupt:
+    u.warning('Interrupt init')
+    exit(0)
+except u.SleepyException as e:
+    u.error(f'==========\n{e}')
+    exit(1)
+except:
+    u.error('Unexpected Error!')
+    raise
+
+
+# --- Functions
+
+
+@app.before_request
+def showip():  # type: ignore / (req: flask.request, msg)
+    '''
+    在日志中显示 ip, 并记录 metrics 信息
+    如 Header 中 User-Agent 为 SleepyPlugin/(每次启动使用随机 uuid) 则不进行任何记录
+
+    :param req: `flask.request` 对象, 用于取 ip
+    :param msg: 信息 (一般是路径, 同时作为 metrics 的项名)
+    '''
+    # --- get path
+    path = flask.request.path
+    # --- log
+    ip1 = flask.request.remote_addr
+    try:
+        ip2 = flask.request.headers['X-Forwarded-For']
+        u.info(f'- Request: {ip1} / {ip2} : {path}')
+    except:
+        ip2 = None
+        u.info(f'- Request: {ip1} : {path}')
+    # --- count
+    if env.util.metrics:
+        d.record_metrics(path)
+
+
+# --- Templates
+
+
+@app.route('/')
+def index():
+    '''
+    根目录返回 html
+    - Method: **GET**
+    '''
+    try:
+        stat = status_list[d.data['status']]
+    except:
+        print(f"索引 {d.data['status']} 超出范围, 使用默认值")
+        stat = {
+            'name': 'Unknown',
+            'desc': '未知的标识符，可能是配置问题。',
+            'color': 'error'
+        }
+    more_text: str = env.page.more_text
+    if env.util.metrics:
+        more_text = more_text.format(
+            visit_today=d.data['metrics']['today'].get('/', 0),
+            visit_month=d.data['metrics']['month'].get('/', 0),
+            visit_year=d.data['metrics']['year'].get('/', 0),
+            visit_total=d.data['metrics']['total'].get('/', 0)
+        )
+    return flask.render_template(
+        'index.html',
+        page_title=env.page.title,
+        page_desc=env.page.desc,
+        page_favicon=env.page.favicon,
+        user=env.page.user,
+        learn_more=env.page.learn_more,
+        repo=env.page.repo,
+        more_text=more_text,
+        hitokoto=env.page.hitokoto,
+        canvas=env.page.canvas,
+        moonlight=env.page.moonlight,
+        lantern=env.page.lantern,
+        mplayer=env.page.mplayer,
+        bg=env.page.background,
+
+        steam_legacy_enabled=env.util.steam_legacy_enabled,
+        steam_enabled=env.util.steam_enabled,
+        steamkey=env.util.steam_key,
+        steamids=env.util.steam_ids,
+
+        status_name=stat['name'],
+        status_color=stat['color'],
+        status_desc=stat['desc'],
+
+        last_updated=d.data['last_updated'],
+    )
+
+
+@app.route('/'+'git'+'hub')
+def git_hub():
+    '''
+    这里谁来了都改不了!
+    '''
+    return flask.redirect('ht'+'tps:'+'//git'+'hub.com/'+'wyf'+'9/sle'+'epy', 301)
+
+
+@app.route('/none')
+def none():
+    '''
+    返回 204 No Content, 可用于 Uptime Kuma 等工具监控服务器状态使用
+    '''
+    return '', 204
+
+
+if env.util.steam_enabled:
+    @app.route('/steam')
+    def steam():
+        return flask.render_template(
+            'steam.html',
+            steamids=env.util.steam_ids
+        )
+
+
+# --- Read-only
+
+
+@app.route('/query')
+def query(ret_as_dict: bool = False):
+    '''
+    获取当前状态
+    - 无需鉴权
+    - Method: **GET**
+
+    :param ret_as_dict: 使函数直接返回 dict 而非 u.format_dict() 格式化后的 response
+    '''
+    st = d.data['status']
+    try:
+        stinfo = status_list[st]
+    except:
+        stinfo = {
+            'id': -1,
+            'name': '164',
+            'desc': '未知的标识符，可能是配置问题。',
+            'color': 'error'
+        }
+    devicelst = d.data['device_status']
+    if d.data['private_mode']:
+        devicelst = {}
+    timenow = datetime.now(pytz.timezone(env.main.timezone))
+    ret = {
+        'time': timenow.strftime('%Y-%m-%d %H:%M:%S'),
+        'timezone': env.main.timezone,
+        'success': True,
+        'status': st,
+        'info': stinfo,
+        'device': devicelst,
+        'device_status_slice': env.status.device_slice,
+        'last_updated': d.data['last_updated'],
+        'refresh': env.status.refresh_interval
+    }
+    if ret_as_dict:
+        return ret
+    else:
+        return u.format_dict(ret)
+
+
+@app.route('/status_list')
+def get_status_list():
+    '''
+    获取 `status_list`
+    - 无需鉴权
+    - Method: **GET**
+    '''
+    stlst = status_list
+    return u.format_dict(stlst)
+
+
+# --- Status API
+
+
+@app.route('/set')
+def set_normal():
+    '''
+    设置状态
+    - http[s]://<your-domain>[:your-port]/set?secret=<your-secret>&status=<a-number>
+    - Method: **GET**
+    '''
+    status = escape(flask.request.args.get('status'))
+    try:
+        status = int(status)
+    except:
+        return u.reterr(
+            code='bad request',
+            message="argument 'status' must be int"
+        )
+    secret = escape(flask.request.args.get('secret'))
+    if secret == env.main.secret:
+        d.dset('status', status)
+        return u.format_dict({
+            'success': True,
+            'code': 'OK',
+            'set_to': status
+        })
+    else:
+        return u.reterr(
+            code='not authorized',
+            message='invaild secret'
+        )
+
+
+# --- Device API
+
+
+@app.route('/device/set', methods=['GET', 'POST'])
+def device_set():
+    '''
+    设置单个设备的信息/打开应用
+    - Method: **GET / POST**
+    '''
+    if flask.request.method == 'GET':
+        try:
+            device_id = escape(flask.request.args.get('id'))
+            device_show_name = escape(flask.request.args.get('show_name'))
+            device_using = u.tobool(escape(flask.request.args.get('using')), throw=True)
+            app_name = escape(flask.request.args.get('app_name'))
+        except:
+            return u.reterr(
+                code='bad request',
+                message='missing param or wrong param type'
+            )
+        secret = escape(flask.request.args.get('secret'))
+        if secret == env.main.secret:
+            devices: dict = d.dget('device_status')
+            if (not device_using) and env.status.not_using:
+                # 如未在使用且锁定了提示，则替换
+                app_name = env.status.not_using
+            devices[device_id] = {
+                'show_name': device_show_name,
+                'using': device_using,
+                'app_name': app_name
+            }
+            d.data['last_updated'] = datetime.now(pytz.timezone(env.main.timezone)).strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            return u.reterr(
+                code='not authorized',
+                message='invaild secret'
+            )
+    elif flask.request.method == 'POST':
+        req = flask.request.get_json()
+        try:
+            secret = req['secret']
+            device_id = req['id']
+            device_show_name = req['show_name']
+            device_using = u.tobool(req['using'], throw=True)
+            app_name = req['app_name']
+        except:
+            return u.reterr(
+                code='bad request',
+                message='missing param'
+            )
+        if secret == env.main.secret:
+            devices: dict = d.dget('device_status')
+            if (not device_using) and env.status.not_using:
+                # 如未在使用且锁定了提示，则替换
+                app_name = env.status.not_using
+            devices[device_id] = {
+                'show_name': device_show_name,
+                'using': device_using,
+                'app_name': app_name
+            }
+            d.data['last_updated'] = datetime.now(pytz.timezone(env.main.timezone)).strftime('%Y-%m-%d %H:%M:%S')
+            d.check_device_status()
+        else:
+            return u.reterr(
+                code='not authorized',
+                message='invaild secret'
+            )
+    else:
+        return u.reterr(
+            code='invaild request',
+            message='This endpoint only supports GET and POST method!'
+        )
+    return u.format_dict({
+        'success': True,
+        'code': 'OK'
+    })
+
+
+@app.route('/device/remove')
+def remove_device():
+    '''
+    移除单个设备的状态
+    - Method: **GET**
+    '''
+    device_id = escape(flask.request.args.get('id'))
+    secret = escape(flask.request.args.get('secret'))
+    if secret == env.main.secret:
+        try:
+            del d.data['device_status'][device_id]
+            d.data['last_updated'] = datetime.now(pytz.timezone(env.main.timezone)).strftime('%Y-%m-%d %H:%M:%S')
+            d.check_device_status()
+        except KeyError:
+            return u.reterr(
+                code='not found',
+                message='cannot find item'
+            )
+    else:
+        return u.reterr(
+            code='not authorized',
+            message='invaild secret'
+        )
+    return u.format_dict({
+        'success': True,
+        'code': 'OK'
+    })
+
+
+@app.route('/device/clear')
+def clear_device():
+    '''
+    清除所有设备状态
+    - Method: **GET**
+    '''
+    secret = escape(flask.request.args.get('secret'))
+    if secret == env.main.secret:
+        d.data['device_status'] = {}
+        d.data['last_updated'] = datetime.now(pytz.timezone(env.main.timezone)).strftime('%Y-%m-%d %H:%M:%S')
+        d.check_device_status()
+    else:
+        return u.reterr(
+            code='not authorized',
+            message='invaild secret'
+        )
+    return u.format_dict({
+        'success': True,
+        'code': 'OK'
+    })
+
+
+@app.route('/device/private_mode')
+def private_mode():
+    '''
+    隐私模式, 即不在 /query 中显示设备状态 (仍可正常更新)
+    - Method: **GET**
+    '''
+    secret = escape(flask.request.args.get('secret'))
+    if secret == env.main.secret:
+        private = u.tobool(escape(flask.request.args.get('private')))
+        if private == None:
+            return u.reterr(
+                code='invaild request',
+                message='"private" arg only supports boolean type'
+            )
+        d.data['private_mode'] = private
+        d.data['last_updated'] = datetime.now(pytz.timezone(env.main.timezone)).strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        return u.reterr(
+            code='not authorized',
+            message='invaild secret'
+        )
+    return u.format_dict({
+        'success': True,
+        'code': 'OK'
+    })
+
+
+@app.route('/save_data')
+def save_data():
+    '''
+    保存内存中的状态信息到 `data.json`
+    - Method: **GET**
+    '''
+    secret = escape(flask.request.args.get('secret'))
+    if secret == env.main.secret:
+        try:
+            d.save()
+        except Exception as e:
+            return u.reterr(
+                code='exception',
+                message=f'{e}'
+            )
+        return u.format_dict({
+            'success': True,
+            'code': 'OK',
+            'data': d.data
+        })
+    else:
+        return u.reterr(
+            code='not authorized',
+            message='invaild secret'
+        )
+
+
+@app.route('/events')
+def events():
+    '''
+    SSE 事件流，用于推送状态更新
+    - Method: **GET**
+    '''
+    def event_stream():
+        last_update = None
+        last_heartbeat = time.time()
+        while True:
+            current_time = time.time()
+            # 检查数据是否已更新
+            current_update = d.data['last_updated']
+
+            # 如果数据有更新，发送更新事件并重置心跳计时器
+            if last_update != current_update:
+                last_update = current_update
+                # 重置心跳计时器
+                last_heartbeat = current_time
+
+                # 获取 /query 返回数据
+                ret = query(ret_as_dict=True)
+                yield f"event: update\ndata: {json5.dumps(ret, quote_keys=True)}\n\n"
+            # 只有在没有数据更新的情况下才检查是否需要发送心跳
+            elif current_time - last_heartbeat >= 30:
+                timenow = datetime.now(pytz.timezone(env.main.timezone))
+                yield f"event: heartbeat\ndata: {timenow.strftime('%Y-%m-%d %H:%M:%S')}\n\n"
+                last_heartbeat = current_time
+
+            time.sleep(1)  # 每秒检查一次更新
+
+    response = flask.Response(event_stream(), mimetype="text/event-stream")
+    response.headers["Cache-Control"] = "no-cache"  # 禁用缓存
+    response.headers["X-Accel-Buffering"] = "no"  # 禁用 Nginx 缓冲
+    return response
+
+
+# --- (Special) Metrics API
+
+
+if env.util.metrics:
+    @app.route('/metrics')
+    def metrics():
+        '''
+        获取统计信息
+        - Method: **GET**
+        '''
+        resp = d.get_metrics_resp()
+        return resp
+
+
+# --- End
+
+
+if __name__ == '__main__':
+    u.info(f'=============== hi {env.page.user}! ===============')
+    print(env.page.background)
+    u.info(f'Starting server: {f"[{env.main.host}]" if ":" in env.main.host else env.main.host}:{env.main.port}{" (debug enabled)" if env.main.flask_debug else ""}')
+    app.run(  # 启↗动↘
+        host=env.main.host,
+        port=env.main.port,
+        debug=env.main.flask_debug
+    )
+    u.info('Server exited, saving data...')
+    d.save()
+    u.info('Bye.')
